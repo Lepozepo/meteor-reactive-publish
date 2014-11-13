8b889048215ca3a975aa5fe67fdde6053987fa9a
Marcelo Reyna <Lepozepo@gmail.com>
2014-11-13 14:04:47 -0600

    Version/Package bump

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
-  summary: "Enable server-side reactivity for Meteor.publish"
+	name:"lepozepo:reactive-publish",
+	summary: "Enable server-side reactivity for Meteor.publish",
+	version:"0.1.7",
+	git: "https://github.com/Lepozepo/meteor-reactive-publish"
 });
 
 Package.on_use(function(api) {
-  api.use([
-    'coffeescript',
-    'deps',
-    'underscore',
-    'server-deps',
-    'accounts-base'
-  ], 'server');
+	api.versionsFrom('METEOR@1.0');
+	api.use([
+		'coffeescript',
+		'tracker',
+		'underscore',
+		'edgee:meteor-server-deps@0.1.2',
+		'accounts-base'
+	], 'server');
 
-  api.add_files('lib/reactive-publish.coffee', 'server');
+	api.add_files('lib/reactive-publish.coffee', 'server');
 });
